3aace09275871b17f02ba3b06dbc3e79f51a7bec
IlijaRa <ilija.rakocevic@finiti.rs>
2024-04-11 01:39:12 +0200

    adjusted front to start mvc pages

--- StudentAdministration/Client/Client.cs
+++ StudentAdministration/Client/Client.cs
@@ -1,16 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Fabric;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
-using Microsoft.ServiceFabric.Data;
+using System.Fabric;
 
 namespace Client
 {
@@ -44,19 +35,33 @@ namespace Client
                                     .UseContentRoot(Directory.GetCurrentDirectory())
                                     .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
                                     .UseUrls(url);
+
                         builder.Services.AddControllersWithViews();
+                        builder.Services.AddRazorPages();
+                        builder.Services.AddMvc(option => option.EnableEndpointRouting = false);
+                        builder.Services.AddHttpClient();
+
                         var app = builder.Build();
-                        if (!app.Environment.IsDevelopment())
+
+                        // Configure the HTTP request pipeline.
+                        if (app.Environment.IsDevelopment())
                         {
-                        app.UseExceptionHandler("/Home/Error");
+                            app.UseDeveloperExceptionPage();
                         }
+                        else
+                        {
+                            app.UseExceptionHandler("/Home/Error");
+                            app.UseHsts();
+                        }
+
                         app.UseStaticFiles();
                         app.UseRouting();
+                        app.UseMvcWithDefaultRoute();
                         app.UseAuthorization();
-                        app.MapControllerRoute(
-                        name: "default",
-                        pattern: "{controller=Home}/{action=Index}/{id?}");
-                        
+
+                        app.MapControllers();
+                        app.Run();
+
                         return app;
 
                     }))
